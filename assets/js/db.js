9d1ededfa9134a671abe25b210a32384c3c19c45
balameow <150638587+balameow@users.noreply.github.com>
2023-11-19 17:40:27 +0800

    Update db.js

--- assets/js/db.js
+++ assets/js/db.js
@@ -30,28 +30,28 @@ function _getData() {
         var data = snapshot.val();
         if (data)
         {
-            var keys = [];
-            var dates = [];
-            var amounts = [];
-            var events = [];
-            var members = [];
+            var allData = [];
             var len = 0;
 
             for (let key in data) {
-                keys.push(key);
-                dates.push(data[key].date);
-                amounts.push(data[key].amount);
-                events.push(data[key].event);
-                members.push(data[key].member);
+                var obj = {
+                    key: key,
+                    date: data[key].date,
+                    amount: data[key].amount,
+                    event: data[key].event,
+                    member: data[key].member
+                };
+                allData.push(obj);
                 len ++;
             }
-            _createPageStr(len, keys, dates, amounts, events, members);
+            allData.sort((a,b) => Date.parse(b.date) - Date.parse(a.date));
+            _createPageStr(len, allData);
         }
     });
 }
 
 // update to page
-function _createPageStr(len, keys, dates, amounts, events, members) {
+function _createPageStr(len, allData) {
     var allLedger = document.querySelector("#allLedger");
     var balanceNum = document.querySelector("#balanceNum");
     var revenueNum = document.querySelector("#revenueNum");
@@ -65,65 +65,78 @@ function _createPageStr(len, keys, dates, amounts, events, members) {
     var cost = 0;
     var revenueTable = [0, 0, 0, 0, 0];
     var costTable = [0, 0, 0, 0, 0];
-    for (i = len-1; i >= 0; i--) {
-        if (amounts[i] >= 0)
+    for (i = 0; i < len; i++) {
+        var useStyle = "";
+        var donateStyle = "";
+        var selectStyle = ["", "", "", "", ""];
+
+        if (allData[i].amount >= 0)
         {
-            money = "+" + amounts[i];
+            money = "+" + allData[i].amount;
             style = "color:green;"
-            revenue += parseInt(amounts[i]);
-            revenueTable = countRevenueCost(revenueTable, members[i], amounts[i]);
+            revenue += parseInt(allData[i].amount);
+            revenueTable = countRevenueCost(revenueTable, allData[i].member, allData[i].amount);
+            donateStyle = "selected";
         }
         else
         {
-            money = amounts[i];
+            money = allData[i].amount;
             style = "color:red;"
-            cost += parseInt(amounts[i]);
-            costTable = countRevenueCost(costTable, members[i], amounts[i]*(-1));
+            cost += parseInt(allData[i].amount);
+            costTable = countRevenueCost(costTable, allData[i].member, Math.abs(allData[i].amount));
+            useStyle = "selected";
         }
+
+        if (allData[i].member.includes("Wade")) selectStyle[0] = "checked";
+        if (allData[i].member.includes("Donna")) selectStyle[1] = "checked";
+        if (allData[i].member.includes("Sammy")) selectStyle[2] = "checked";
+        if (allData[i].member.includes("Ken")) selectStyle[3] = "checked";
+        if (allData[i].member.includes("Sandy")) selectStyle[4] = "checked";
+
         str += `
             <div class="card border border-dark">
                 <div class="card-body">
-                    <h5 class="card-text fs-5 fw-bold">${events[i]}</h5>
+                    <h5 class="card-text fs-5 fw-bold">${allData[i].event}</h5>
                     <h5 class="card-text fw-bold" style="position: absolute; top: 20px; right: 5px; text-align:center; font-size: 22px; ${style}">${money}</h5>
-                    <h6 class="card-text text-muted" style="position: absolute; top: 3px; left: 5px; text-align:right; font-size: 12px;">${dates[i]}</h6>
-                    <h6 class="card-text text-muted" style="position: absolute; top: 25px; left: 5px; text-align:right; font-size: 9px;">${members[i]}</h6>
-                    <i data-bs-toggle="modal" data-bs-target="#checkModal" class="bx bx-edit bx-xs bx-tada-hover text-primary" style="position: absolute; bottom: 1px; left: 1px;"></i>
+                    <h6 class="card-text text-muted" style="position: absolute; top: 3px; left: 5px; text-align:right; font-size: 12px;">${allData[i].date}</h6>
+                    <h6 class="card-text text-muted" style="position: absolute; top: 25px; left: 5px; text-align:right; font-size: 9px;">${allData[i].member}</h6>
+                    <i data-bs-toggle="modal" data-bs-target="#checkModal-${allData[i].key}" class="bx bx-edit bx-xs bx-tada-hover text-primary" style="position: absolute; bottom: 1px; left: 1px;"></i>
                 </div>
             </div>
-            <div class="modal fade" id="checkModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="manipulateFundLabel" aria-hidden="true">
+            <div class="modal fade" id="checkModal-${allData[i].key}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="checkModal-${allData[i].key}Label" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="checkModalLabel">更新/刪除</h5>
+                            <h5 class="modal-title" id="checkModal-${allData[i].key}Label">更新/刪除</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <select id="update_tag" class="form-select form-select-sm mb-3" aria-label=".form-select-sm example">
-                                <option value="update_use">我要使用</option>
-                                <option value="update_donate">我要捐款</option>
+                            <select id="update_tag-${allData[i].key}" class="form-select form-select-sm mb-3" aria-label=".form-select-sm example">
+                                <option value="update_use-${allData[i].key}" ${useStyle}>我要使用</option>
+                                <option value="update_donate-${allData[i].key}" ${donateStyle}>我要捐款</option>
                             </select>
                             <div class="form-check mb-3" style="float:left;">
-                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Wade"> Wade</label>
-                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Donna"> Donna</label>
-                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Sammy"> Sammy</label>
-                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Ken"> Ken</label>
-                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Sandy"> Sandy</label>
+                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Wade-${allData[i].key}" ${selectStyle[0]}> Wade</label>
+                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Donna-${allData[i].key}" ${selectStyle[1]}> Donna</label>
+                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Sammy-${allData[i].key}" ${selectStyle[2]}> Sammy</label>
+                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Ken-${allData[i].key}" ${selectStyle[3]}> Ken</label>
+                                <label style="margin: 0px 15px;"><input class="form-check-input" type="checkbox" value="" id="update_Sandy-${allData[i].key}" ${selectStyle[4]}> Sandy</label>
                             </div>
                             <div class="mb-3" style="float:left;">
-                                <input type="date" id="update_date" name="date">
+                                <input type="date" id="update_date-${allData[i].key}" name="date">
                             </div>
                             <div class="input-group input-group-sm mb-3">
-                                <input type="number" class="form-control" id="update_money" step="100" min="0" max="10000000">
+                                <input type="number" class="form-control" id="update_money-${allData[i].key}" step="100" min="0" max="10000000" value="${Math.abs(money)}">
                             </div>
-                            <form id="update_myForm">
+                            <form id="update_myForm-${allData[i].key}">
                                 <div class="mb-3">
-                                    <input type="text" class="form-control" id="update_note" maxlength="20">
+                                    <input type="text" class="form-control" id="update_note-${allData[i].key}" maxlength="20" value="${allData[i].event}">
                                 </div>
                             </form>
                         </div>
                         <div class="modal-footer">
-                        <button onclick="updateEvent('${keys[i]}')" type="button" class="btn btn-dark" data-bs-dismiss="modal">更新</button>
-                            <button onclick="deleteEvent('${keys[i]}')" type="button" class="btn btn-dark" data-bs-dismiss="modal">刪除</button>
+                        <button onclick="updateEvent('${allData[i].key}')" type="button" class="btn btn-dark" data-bs-dismiss="modal">更新</button>
+                            <button onclick="deleteEvent('${allData[i].key}')" type="button" class="btn btn-dark" data-bs-dismiss="modal">刪除</button>
                         </div>
                     </div>
                 </div>
@@ -198,17 +211,17 @@ function manipulateFund()
 // updateEvent
 function updateEvent(key)
 {
-    var tag = $('#update_tag').val();
-    var date = $('#update_date').val();
-    var money = $('#update_money').val();
-    var note = $('#update_note').val();
+    var tag = $(`#update_tag-${key}`).val();
+    var date = $(`#update_date-${key}`).val();
+    var money = $(`#update_money-${key}`).val();
+    var note = $(`#update_note-${key}`).val();
     var membersGroup = "";
-    if(document.getElementById('update_Wade').checked) membersGroup += "Wade,";
-    if(document.getElementById('update_Donna').checked) membersGroup += "Donna,";
-    if(document.getElementById('update_Sammy').checked) membersGroup += "Sammy,";
-    if(document.getElementById('update_Ken').checked) membersGroup += "Ken,";
-    if(document.getElementById('update_Sandy').checked) membersGroup += "Sandy,";
-    if (tag == "update_use") money *= -1;
+    if(document.getElementById(`update_Wade-${key}`).checked) membersGroup += "Wade,";
+    if(document.getElementById(`update_Donna-${key}`).checked) membersGroup += "Donna,";
+    if(document.getElementById(`update_Sammy-${key}`).checked) membersGroup += "Sammy,";
+    if(document.getElementById(`update_Ken-${key}`).checked) membersGroup += "Ken,";
+    if(document.getElementById(`update_Sandy-${key}`).checked) membersGroup += "Sandy,";
+    if (tag == "update_use-${key}") money *= -1;
     if (date == "") date = _DateTimezone(8);
 
     membersGroup = membersGroup.slice(0, -1);
@@ -221,7 +234,7 @@ function updateEvent(key)
         db.ref(`/ledger/${key}/member`).set(membersGroup);
     }
     _getData();
-    $('#myForm')[0].reset();
+    $('#update_myForm-${key}')[0].reset();
 }
 
 // delete event
